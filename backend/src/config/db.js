200ecd0064e389df5928e9dfb7fa752a0635bf32
ntao <tao.ngo@ynov.com>
2026-04-29 14:46:27 +0200

    db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,6 +1,6 @@
  import { setServers } from 'node:dns';
 
-// Force l'utilisation des DNS Google et Cloudflare
+// Force l'utilisation des DNS Google et Cloudflare   e
 setServers(['8.8.8.8', '1.1.1.1']);
 
 // Ensuite, ton code existant :
@@ -14,4 +14,4 @@ export const connectDB = async () => {
     console.error('❌ MongoDB connection error:', error);
     process.exit(1);
   }
-};
+};   
