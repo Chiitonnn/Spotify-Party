f179b00ee3cbc4c06d028dda2eea1874516fc873
ntao <tao.ngo@ynov.com>
2026-04-29 14:44:47 +0200

    db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,11 +1,14 @@
+ import { setServers } from 'node:dns';
+
+// Force l'utilisation des DNS Google et Cloudflare
+setServers(['8.8.8.8', '1.1.1.1']);
+
+// Ensuite, ton code existant :
 import mongoose from 'mongoose';
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log('✅ MongoDB connected');
   } catch (error) {
     console.error('❌ MongoDB connection error:', error);
